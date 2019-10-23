91e3fee2d733d9f49a97683c6e8982a33746237f
dhaivat7 <48772054+dhaivat7@users.noreply.github.com>
2019-10-22 21:26:04 -0700

    Create calci_pkg.sv

---
+++ calci_pkg.sv
@@ -0,0 +1,16 @@
+package calci_pkg;
+	typedef virtual calci_if calci_vif ;
+	`include "calci_defines.sv"
+	`include "calci_trans.sv"
+	`include "calci_config.sv"
+	`include "calci_gen.sv"
+	`include "calci_driver.sv"
+	`include "calci_sig_cov.sv"
+	`include "calci_monitor.sv"
+	`include "calci_ref_model.sv"
+	`include "calci_trans_cov.sv"
+	`include "calci_sb.sv"
+	`include "calci_env.sv"
+	`include "calci_test.sv"
+//------------------------------------------------  
+endpackage
